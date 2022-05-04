6170424ebf31f21a1ee71be213474108154b4372
four-707 <103592889+four-707@users.noreply.github.com>
2022-05-04 18:50:39 +0530

    Add files via upload

---
+++ components.js
@@ -0,0 +1,50 @@
+// LOAD BG IMAGE
+// background images
+
+const BG_IMG1 = new Image();
+BG_IMG1.src = "background/blue.jpg";
+
+const BG_IMG2 = new Image();
+BG_IMG2.src ="background/desert.jpg";
+
+const BG_IMG3 = new Image();
+BG_IMG3.src = "background/green3.jpg";
+
+const BG_IMG4 = new Image();
+BG_IMG4.src = "background/dark.jpg";
+
+
+// OTGER IMAGES
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "img/level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "img/life.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "img/score.png";
+
+
+/////// END LOAD IMAGES ////////
+
+// ************************ //
+
+/////// LOAD SOUNDS ////////
+
+const WALL_HIT = new Audio();
+WALL_HIT.src = "sounds/wall.mp3";
+
+const LIFE_LOST = new Audio();
+LIFE_LOST.src = "sounds/life_lost.mp3";
+
+const PADDLE_HIT = new Audio();
+PADDLE_HIT.src = "sounds/paddle_hit.mp3";
+
+const WIN = new Audio();
+WIN.src = "sounds/win.mp3";
+
+const BRICK_HIT = new Audio();
+BRICK_HIT.src = "sounds/brick_hit.mp3";
+
+
+/////// END LOAD SOUNDS ////////
